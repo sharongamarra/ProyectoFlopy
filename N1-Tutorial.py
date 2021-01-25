e32f08c84727b4c0c4149f75244a45f418236101
Sharon Gamarragit config --global user.name Sharon <sharon2172542@correo.uis.edu.co>
2021-01-25 09:58:35 -0500

    Añadir TDIS

--- N1-Tutorial.py
+++ N1-Tutorial.py
@@ -12,9 +12,12 @@ N = 101
 L = 400.0
 H = 50.0
 k = 1.0
-#Crear objeto de simulación
+#Crear objeto de simulación (busca la carpeta especializada para guardar los archivos)
 sim = flopy.mf6.MFSimulation(
-    sim_name=name, exe_name="mf6", version="mf6", sim_ws="."
+    sim_name=name, exe_name="C:\12-DIPLOMADO\Programas\mf6.2.0\bin", version="mf6", sim_ws="workspace"
 )
 
-
+#Crea el TDIS objeto Flopy
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)]
+)
