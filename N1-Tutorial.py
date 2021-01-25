0ca772169978452831f284fbb3b48038c82e4ce7
Sharon Gamarragit config --global user.name Sharon <sharon2172542@correo.uis.edu.co>
2021-01-25 09:43:18 -0500

    AGREGUE DATOS AL MODELO

--- N1-Tutorial.py
+++ N1-Tutorial.py
@@ -3,5 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import flopy
 
+#Datos del modelo
+name = "tutorial01_mf6"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
 
 
