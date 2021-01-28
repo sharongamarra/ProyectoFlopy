9084eddcecb5ea83fe1653b5e2d7bcf8409cf5f6
Sharon Gamarragit config --global user.name Sharon <sharon2172542@correo.uis.edu.co>
2021-01-28 17:38:39 -0500

    CAMBIAR PERMEABILIDAD

--- N1-Tutorial.py
+++ N1-Tutorial.py
@@ -48,6 +48,8 @@ start = h1 * np.ones((Nlay, N, N))
 ic = flopy.mf6.ModflowGwfic(gwf, pname="ic", strt=start)
 
 #Crear el NPF paquete de flujo
+k=np.ones([10,N,N])
+k[1,:,:]=5e-3
 npf = flopy.mf6.ModflowGwfnpf(gwf, icelltype=1, k=k, save_flows=True)
 
 #Crear el CHD paquete de cabeza constante
